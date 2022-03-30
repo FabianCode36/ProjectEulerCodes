66ae2911d15828c32ef118107108d8ca5de92fd9
FabianCode36 <82763746+FabianCode36@users.noreply.github.com>
2022-03-30 08:21:55 +0200

    Update Project_Euler_3.cpp

--- Project_Euler_3/Project_Euler_3.cpp
+++ Project_Euler_3/Project_Euler_3.cpp
@@ -9,21 +9,21 @@
 
 using namespace std;
 
-const long long SIZE = 25;
+const long long TEST_VALUE = 12;
 const long long Value = 600851475143;
 
 void SiebDesEratosthenes(const long long Size, list<long long>& Prim) {
 	
-	vector<bool> isPrim (Size, true);
+	vector<bool> isPrim (Size+1, true);
 
 	isPrim.at(0) = false;
 	isPrim.at(1) = false;
 
-	for (long i = 2; i < Size; i++) {
+	for (long i = 2; i < Size+1; i++) {
 		if (isPrim[i]) {
 			Prim.push_back(i);
 		}
-		for (long j = 2 * i; j < Size; j = j + i) {
+		for (long j = 2 * i; j < Size+1; j = j + i) {
 			isPrim[j] = false;
 		}
 	}
@@ -31,10 +31,17 @@ void SiebDesEratosthenes(const long long Size, list<long long>& Prim) {
 
 void PrimFaktorZerlegung(const long long Size, list<long long>& PrimFaktoren, list<long long>& Prim) {
 
-	long long root = sqrt(Size); // Wurzel aus 25
+	// Test, ob Zahl nicht selber eine Primzahl ist
+	for (auto cnt = Prim.begin(); cnt != Prim.end(); cnt++) {
+		if (Size == *cnt) {
+			PrimFaktoren.push_back(*cnt);
+			return;
+		}
+	}
 
+	// Wenn Zahl keine Primzahl, dann die einzelnen Primfaktoren finden
 	for (auto it = Prim.begin(); it != Prim.end(); ++it) {
-		if (*it <= root) {
+		if (*it <= Size) {
 			if (Size % (*it) == 0) {
 				PrimFaktoren.push_back(*it);
 			}
@@ -67,23 +74,23 @@ int main()
 	list<long long>Primzahlen;
 	list<long long>PrimFaktoren;
 
-	SiebDesEratosthenes(SIZE, Primzahlen);
+	SiebDesEratosthenes(TEST_VALUE, Primzahlen);
 
 	for (auto it = Primzahlen.begin(); it != Primzahlen.end(); it++) {
 		cout << *it << " ";
 	}
 	cout << endl;
 
-	PrimFaktorZerlegung(SIZE, PrimFaktoren, Primzahlen);
-	cout << "Primfaktor von " << SIZE << " sind ";
-	for (auto it = PrimFaktoren.begin(); it != PrimFaktoren.end(); ++it) {
+	PrimFaktorZerlegung(TEST_VALUE, PrimFaktoren, Primzahlen);
+	cout << "Primfaktor von " << TEST_VALUE << " sind ";
+	for (auto it = PrimFaktoren.begin(); it != PrimFaktoren.end(); it++) {
 		cout << *it << " ";
 	}
 
 	cout << endl;
 
 	long MaxPrim = PrimFaktoren.back();
-	cout << "Groesster Primfaktor von " << SIZE << " ist " << MaxPrim << endl;
+	cout << "Groesster Primfaktor von " << TEST_VALUE << " ist " << MaxPrim << endl;
 
 	cout << "Groesster Primfaktor von " << Value << " ist " << MaxPrimFaktor(Value) << endl;
 
